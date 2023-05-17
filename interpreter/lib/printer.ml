8c7dfb73f19c6648b0db8a8a70e6ad3c32c50c0b
Denis Enes <denisenes68@gmail.com>
2023-05-18 01:21:19 +0700

    printer refactoring

--- interpreter/lib/printer.ml
+++ interpreter/lib/printer.ml
@@ -1,31 +1,28 @@
 open Common
 open Utils
 
-let rec print_sexpression sexpr =
+let rec value_to_string (sexpr : value) : string =
   match sexpr with
-    | Fixnum(value)  -> print_int value
-    | Boolean(b) -> print_string (if b then "#t" else "#f")
-    | Symbol(name) -> print_string name
+    | Fixnum(value)  -> string_of_int value
+    | Boolean(b) -> if b then "#t" else "#f"
+    | Symbol(name) -> name
     | Pair(_ , _) -> 
-      print_string "(";
-      if is_list sexpr then print_list sexpr else print_pair sexpr;
-      print_string ")"
+      "(" ^
+      (if is_list sexpr then list_to_string sexpr else pair_to_string sexpr)
+      ^ ")"
 
-    | Primitive (name,  _) -> print_string ("<" ^ name ^ ">")
-    | Nil -> print_string "nil"
+    | Primitive (name,  _) -> "<" ^ name ^ ">"
+    | Nil -> "nil"
 
-and print_list list = 
+and list_to_string (list : value) : string = 
   match list with
-    | Pair(car, Nil) -> print_sexpression car
-    | Pair(car, cdr) -> 
-      print_sexpression car;
-      print_string " ";
-      print_list cdr;
+    | Pair(car, Nil) -> value_to_string car
+    | Pair(car, cdr) -> value_to_string car ^ " " ^ list_to_string cdr
     | _ -> raise (PrintError("Something went wrong while printing list"))
 
-and print_pair pair = match pair with
-  | Pair (car, cdr) -> 
-    print_sexpression car;
-    print_string " . ";
-    print_sexpression cdr;
+and pair_to_string pair = match pair with
+  | Pair (car, cdr) -> value_to_string car ^ " . " ^ value_to_string cdr
   | _ -> raise (PrintError("Something went wrong while printing pair"))
+
+let print_value (sexpr : value) : unit =
+  print_endline (value_to_string sexpr)
